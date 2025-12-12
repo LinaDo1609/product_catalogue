cb85f54e16ec8f9fe50d3a4a874edd07fcc24459
LinaDo <linado0916@gmail.com>
2025-12-12 16:06:26 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
-import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import s from './App.module.scss';
+import { Header } from './components/Header';
+import { HomePage } from './pages/HomePage/HomePage';
+import { Catalogue } from './pages/Catalogue/Catalogue';
+import { NotFound } from './pages/NotFound/NotFound';
+import { Footer } from './components/Footer';
+import { ProductDetailsPage } from './pages/ProductDetailsPage';
+import { Cart } from './pages/Cart/Cart';
+import { Favourite } from './pages/Favourite';
+import { ScrollToTop } from './components/ScrollTop/ScrollTop';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className={s.app}>
+      <h1 className={s.app__title}>Product Catalog</h1>
+
+      <ScrollToTop />
+      <Header />
+
+      <div className={s.app__main}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/phones"
+            element={<Catalogue title={'Mobile phones'} type={'phones'} />}
+          />
+          <Route
+            path="/tablets"
+            element={<Catalogue title={'Tablets'} type={'tablets'} />}
+          />
+          <Route
+            path="/accessories"
+            element={<Catalogue title={'Accessories'} type={'accessories'} />}
+          />
+          <Route
+            path="/:category/:productId"
+            element={<ProductDetailsPage />}
+          />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/favourite" element={<Favourite />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </div>
+
+      <Footer />
     </div>
   );
 };
+
+//
